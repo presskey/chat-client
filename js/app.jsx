71dcc6bb1038aaa055ce1987243bf3b92ea433b0
Alex Gusev <hello.from.code@gmail.com>
2015-10-06 18:22:43 +0300

    Build component structure

--- js/app.jsx
+++ js/app.jsx
@@ -1,6 +1,40 @@
 class Chat extends React.Component {
   render() {
-    return <div>chat</div>;
+    return (
+      <div>
+        <MessageList />
+        <MessageForm />
+      </div>
+    );
+  }
+}
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <ul>
+        <Message text="message1" />
+        <Message text="message2" />
+        <Message text="message3" />
+      </ul>
+    );
+  }
+}
+
+class MessageForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Type a message here" />
+        <input type="submit" value="Send" />
+      </form>
+    );
+  }
+}
+
+class Message extends React.Component {
+  render() {
+    return <li>{this.props.text}</li>;
   }
 }
 
